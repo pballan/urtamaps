0e9e1c312e04a1494f74ad054d701809733ca201
pballan <pballan@dc.uba.ar>
2019-05-16 10:07:30 -0300

    drogas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Drogas v-bind:drogas="drogas" v-on:del-droga="deleteDroga"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Drogas from './components/Drogas.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Drogas
+  },
+  data(){
+    return {
+      drogas: [
+        {
+          id: 1,
+          title: "paco",
+          completed: true
+        },{
+          id: 2,
+          title: "fafafa",
+          completed: true
+        },{
+          id: 3,
+          title: "fasuli",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteDroga(id) {
+      this.drogas = this.drogas.filter(droga => droga.id !== id);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/DrogaDetail.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="droga-detail" v-bind:class="{'is-complete': droga.completed }">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{droga.title}}
+            <button @click="$emit('del-droga', droga.id)" class="del">x</button>
+
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DrogaDetail",
+    props: ["droga"],
+    methods: {
+        markComplete(){
+            this.droga.completed = !this.droga.completed
+        }
+    }
+}
+</script>
+
+<style scoped>
+.droga-detail {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+.is-complete{
+    text-decoration: line-through;
+}
+
+.del {
+    background: red;
+    color: white;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+}
+
+</style>
+
